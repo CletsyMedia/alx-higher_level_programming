c16b55de5c658bfd8eddf03a44674de49bd9720c
CletsyMedia <Cletussam12@yahoo.com>
2023-10-02 19:11:56 +0100

    More concatanation

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str.split(" ")[-11][:-1] + "d with Python")
-
+str = str[39:-63] + str[106:-17] + str[:6]
+print(str)

89130cbb2e33115174cf895bad534392b89897c6
CletsyMedia <Cletussam12@yahoo.com>
2023-10-02 19:08:39 +0100

    More concatanation

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+print(str.split(" ")[-11][:-1] + "d with Python")
+

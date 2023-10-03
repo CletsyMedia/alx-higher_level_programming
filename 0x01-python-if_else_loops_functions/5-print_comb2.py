aca91ade1cc4484cbc6e3973f2a99d0d76828a32
CletsyMedia <Cletussam12@yahoo.com>
2023-10-03 17:22:57 +0100

    Prints numbers from 0 to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -4,9 +4,11 @@ for a in range(100):
     # Check if the current number is less than 10 (single-digit number).
     if a < 10:
         # If the number is less than 10, print it with a leading zero.
-        # Use end=", " if it's not the last number; otherwise, use end="\n" to start a new line.
+        # Use end=", " if it's not the last number; otherwise,
+        # use end="\n" to start a new line.
         print("0{}".format(a), end=", " if a != 99 else "\n")
     else:
         # If the number is 10 or greater, print it without a leading zero.
-        # Use end=", " if it's not the last number; otherwise, use end="\n" to start a new line.
+        # Use end=", " if it's not the last number; otherwise,
+        # use end="\n" to start a new line.
         print(a, end=", " if a != 99 else "\n")

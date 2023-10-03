1b31529edaa84302ab066f2f22198b23abaf9b5c
CletsyMedia <Cletussam12@yahoo.com>
2023-10-03 15:07:28 +0100

    Printing in Hex format

---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Iterate through numbers from 0 to 98
+for i in range(99):
+    # Print the current number in decimal and hexadecimal format
+    print("{} = 0x{:x}".format(i, i))

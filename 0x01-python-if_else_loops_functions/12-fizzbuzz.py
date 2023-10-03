01c23a8106d05c5237838e7102c18bf60cce7bac
CletsyMedia <Cletussam12@yahoo.com>
2023-10-03 22:00:30 +0100

    Fizz Buzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def fizzbuzz():
     for num in range(1, 101):
         if num % 3 == 0 and num % 5 == 0:
@@ -9,6 +10,7 @@ def fizzbuzz():
             print("Buzz", end=" ")
         else:
             print(num, end=" ")
+    print()  # Add a newline after printing the sequence.
 
 
 # Call the fizzbuzz function to print the sequence.

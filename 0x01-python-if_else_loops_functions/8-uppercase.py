6539a1984a4dadb94e4a335e4a1c6d06ddc8671b
CletsyMedia <Cletussam12@yahoo.com>
2023-10-03 20:08:58 +0100

    Converting lower to UPPER

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
-
 def uppercase(str):
-    """
-    Prints the input string in uppercase followed by a new line.
-
-    :param str: The input string.
-    """
-    for char in str:
-        # Check if the character is a lowercase letter.
-        if 'a' <= char <= 'z':
-            # Convert it to uppercase using ASCII values.
-            char = chr(ord(char) - ord('a') + ord('A'))
-        print(char, end="")  # Print each character without a newline
-    print('')  # Print a newline after processing the entire string
+    for upper in str:
+        if ord(upper) <= ord('z') and ord(upper) >= ord('a'):
+            ch = chr(ord(upper) - 32)
+        else:
+            ch = upper
+        print("{:s}".format(ch), end="")
+    print('')

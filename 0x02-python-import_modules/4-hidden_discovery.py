0024fe2ded03d61dde8db817e3e0138c77afc13b
CletsyMedia <Cletussam12@yahoo.com>
2023-10-05 23:35:52 +0100

    Compiled module

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,12 +1,7 @@
+#!/usr/bin/python3
 if __name__ == "__main__":
-    import hidden_4
-
-    # Get the names defined in the module
-    names = dir(hidden_4)
-
-    # Filter and sort the names
-    filtered_names = sorted([name for name in names if not name.startswith("__")])
-
-    # Print the names, one per line
-    for name in filtered_names:
-        print(name)
+    from hidden_4 import *
+    arr = dir()
+    for i in range(0, len(arr)):
+        if arr[i][0:2] != "__":
+            print("{}".format(arr[i]))

8ee1cfa0c2643885592ba967e02b44d40f3d1261
CletsyMedia <Cletussam12@yahoo.com>
2023-10-05 23:59:26 +0100

    Python bytecode

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,22 @@
+def magic_calculation(a, b):
+    # Initialize add and sub to None
+    add, sub = None, None
+
+    # Check if a is less than b
+    if a < b:
+        # Import the 'add' and 'sub' functions from 'magic_calculation_102' module
+        add = __import__('magic_calculation_102').add
+        sub = __import__('magic_calculation_102').sub
+
+        # Perform the 'add' operation with a and b
+        c = add(a, b)
+
+        # Loop from 4 to 5 (inclusive) and add each value to 'c'
+        for i in range(4, 6):
+            c = add(c, i)
+
+        # Return the final result
+        return c
+
+    # If a is not less than b, perform the 'sub' operation and return the result
+    return sub(a, b)

9f006490383d918645daba84bc144b70baf24d69
CletsyMedia <Cletussam12@yahoo.com>
2023-10-05 18:53:18 +0100

    Imports functions from the file calculator_1.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-from cal import add, sub, mul, div
-if __name__ == "__main__":
-  a = 10
-  b = 5
+from calculator_1 import add, sub, mul, div
 
-  res_add = add(a, b)
-  res_sub = sub(a, b)
-  res_mul = mul(a, b)
-  res_div = div(a, b)
-  
-  print("{:d} + {:d} = {:d}".format(a, b, res_add))
-  print("{:d} - {:d} = {:d}".format(a, b, res_sub))
-  print("{:d} * {:d} = {:d}".format(a, b, res_mul))
-  print("{:d} / {:d} = {:d}".format(a, b, res_div))
+if __name__ == "__main__":
+    a = 10
+    b = 5
+
+    res_add = add(a, b)
+    res_sub = sub(a, b)
+    res_mul = mul(a, b)
+    res_div = div(a, b)
+
+    print("{:d} + {:d} = {:d}".format(a, b, res_add))
+    print("{:d} - {:d} = {:d}".format(a, b, res_sub))
+    print("{:d} * {:d} = {:d}".format(a, b, res_mul))
+    print("{:d} / {:d} = {:d}".format(a, b, res_div))

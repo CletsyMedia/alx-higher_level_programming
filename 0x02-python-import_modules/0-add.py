41e4f9263a57e6fed4620ca95f858a03d7217f27
CletsyMedia <Cletussam12@yahoo.com>
2023-10-05 15:22:14 +0100

    Imports functions from the file calculator_1.py

--- 0x02-python-import_modules/0-add.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-from add_0 import add
-
-if __name__ == "__main__":
-    a = 1
-    b = 2
-    result = add(a, b)
-    print("{:d} + {:d} = {:d}".format(a, b, result))
--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 from cal import add, sub, mul, div
+if __name__ == "__main__":
+  a = 10
+  b = 5
 
-a = 10
-b = 5
-
-res_add = add(a, b)
-res_sub = sub(a, b)
-res_mul = mul(a, b)
-res_div = div(a, b)
-
-print("{:d} + {:d} = {:d}".format(a, b, res_add))
-print("{:d} - {:d} = {:d}".format(a, b, res_sub))
-print("{:d} * {:d} = {:d}".format(a, b, res_mul))
-print("{:d} / {:d} = {:d}".format(a, b, res_div))
+  res_add = add(a, b)
+  res_sub = sub(a, b)
+  res_mul = mul(a, b)
+  res_div = div(a, b)
+  
+  print("{:d} + {:d} = {:d}".format(a, b, res_add))
+  print("{:d} - {:d} = {:d}".format(a, b, res_sub))
+  print("{:d} * {:d} = {:d}".format(a, b, res_mul))
+  print("{:d} / {:d} = {:d}".format(a, b, res_div))

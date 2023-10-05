6ac69969c422756eff7f71ab109b701ff5f8e5e6
CletsyMedia <Cletussam12@yahoo.com>
2023-10-05 15:19:03 +0100

    Imports functions from the file calculator_1.py

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from cal import add, sub, mul, div
+
+a = 10
+b = 5
+
+res_add = add(a, b)
+res_sub = sub(a, b)
+res_mul = mul(a, b)
+res_div = div(a, b)
+
+print("{:d} + {:d} = {:d}".format(a, b, res_add))
+print("{:d} - {:d} = {:d}".format(a, b, res_sub))
+print("{:d} * {:d} = {:d}".format(a, b, res_mul))
+print("{:d} / {:d} = {:d}".format(a, b, res_div))

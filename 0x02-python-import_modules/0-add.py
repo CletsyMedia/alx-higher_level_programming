492a9e240d763a2a2234121b834696d4c7facd9c
CletsyMedia <Cletussam12@yahoo.com>
2023-10-05 14:42:28 +0100

    importing a func from another file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-from add_0 import add
 if __name__ == "__main__":
-a = 1
-b = 2
-result = add(a, b)
-print("{:d} + {:d} = {:d}".format(a, b, result))
+    from add_0 import add
+    a = 1
+    b = 2
+    result = add(a, b)
+    print("{:d} + {:d} = {:d}".format(a, b, result))

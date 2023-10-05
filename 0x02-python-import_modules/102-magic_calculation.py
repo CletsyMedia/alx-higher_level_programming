872da77d7200c2176ce1425b87b034bb2b7637a7
CletsyMedia <Cletussam12@yahoo.com>
2023-10-06 00:08:40 +0100

    Python bytecode

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,16 +1,20 @@
+#!/usr/bin/python3
 def magic_calculation(a, b):
-    # Initialize 'add' and 'sub' variables to None.
-    add = None
-    sub = None
+    # Import 'add' and 'sub' functions from 'magic_calculation_102' module.
+    from magic_calculation_102 import add, sub
 
     # Check if 'a' is less than 'b'.
     if a < b:
-        # Import 'add' and 'sub' functions from 'magic_calculation_102'.
-        from magic_calculation_102 import add, sub
+        # Initialize 'c' to the result of adding 'a' and 'b'.
+        c = add(a, b)
 
-        # Call 'add' function with arguments 'a' and 'b'.
-        return add(a, b)
+        # Iterate over a range from 4 to 6 (inclusive).
+        for i in range(4, 7):
+            # Add 'i' to 'c' and store the result in 'c'.
+            c = add(c, i)
+
+        # Return the final result 'c'.
+        return c
     else:
-        # If 'a' is greater than or equal to 'b', call 'sub'
-        # function with arguments 'a' and 'b'.
+        # If 'a' is greater than or equal to 'b', subtract 'b' from 'a' and return the result.
         return sub(a, b)

abc2a0fbad3c28b4aaf2763cce03e06b8e2aaa5f
CletsyMedia <Cletussam12@yahoo.com>
2023-10-07 07:59:59 +0100

    Write a function that adds 2 tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    # Extract the first two elements from each tuple or use 0 if they don't exist
+    # Extract the first two elements from each tuple
+    # or use 0 if they don't exist
     a1 = tuple_a[0] if len(tuple_a) > 0 else 0
     a2 = tuple_a[1] if len(tuple_a) > 1 else 0
     b1 = tuple_b[0] if len(tuple_b) > 0 else 0

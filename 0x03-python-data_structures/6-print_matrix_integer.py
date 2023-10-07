4c57f18f8db577b75a31635b9b276cd5d108850b
CletsyMedia <Cletussam12@yahoo.com>
2023-10-07 07:56:39 +0100

    Lists of lists = Matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,23 +1,6 @@
 #!/usr/bin/python3
-
 def print_matrix_integer(matrix=[[]]):
-    """
-    Print a matrix of integers in the specified format.
-
-    Args:
-        matrix (list of lists): The matrix of integers to be printed.
-
-    Format:
-        Each row is printed on a separate line, with integers separated by spaces.
-
-    Example:
-        For matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]:
-        Output:
-            1 2 3
-            4 5 6
-            7 8 9
-
-    If the matrix is empty, an empty line is printed.
-    """
     for row in matrix:
         print(" ".join("{:d}".format(num) for num in row))
+    if not matrix:
+        print()

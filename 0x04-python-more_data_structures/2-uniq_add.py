95923e8288cf984ad945dc58bfed9108fc067309
CletsyMedia <Cletussam12@yahoo.com>
2023-10-11 12:11:08 +0100

    Adds all unique integers in a list

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    # Create a set from the list to automatically remove duplicates
     unique_set = set(my_list)
-    # Use the sum function to add all unique integers
     total = sum(unique_set)
-   # Return the sum of unique integers
     return total

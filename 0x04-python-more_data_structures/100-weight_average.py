11a3ff32cdaf5a56be5fa03685721f312f46186c
CletsyMedia <Cletussam12@yahoo.com>
2023-10-11 19:41:05 +0100

    Weighted average of all integers tuple

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0.0
+
+    total_score = sum(score * weight for score, weight in my_list)
+    total_weight = sum(weight for _, weight in my_list)
+
+    return total_score / total_weight

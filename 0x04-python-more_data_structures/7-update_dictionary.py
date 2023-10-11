29ef8e4b3d8bc817a1d0dcf8ab513d3fd2b13b68
CletsyMedia <Cletussam12@yahoo.com>
2023-10-11 17:32:18 +0100

    Replaces or adds key/value in a dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
+    updated = False  # Flag to track whether the key was updated or added
+    for k in a_dictionary:
+        if k == key:
+            a_dictionary[k] = value  # Update the value if the key exists
+            updated = True
+    if not updated:
+        a_dictionary[key] = value  # Add the key and value if it doesn't exist
     return a_dictionary
-

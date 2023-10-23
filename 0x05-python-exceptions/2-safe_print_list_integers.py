73a23c3c0111f691893b17df252fbb2a2e02b18d
CletsyMedia <Cletussam12@yahoo.com>
2023-10-23 12:22:58 +0100

    Pints the first x elements of a list and only integers

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,15 +2,11 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    try:
-        for i in range(x):
-            if i >= len(my_list):
-                raise IndexError("list index out of range")
-            if isinstance(my_list[i], int):
-                print("{:d}".format(my_list[i]), end="")
-                count += 1
-        print()
-    except (ValueError, TypeError, IndexError) as e:
-        print("{}".format(e))
-
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (ValueError, TypeError):
+            continue
+    print("")
     return count

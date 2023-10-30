75e0d30f448a14a0075a8818d60285bd1b103316
CletsyMedia <Cletussam12@yahoo.com>
2023-10-30 18:43:35 +0100

    Detect instance deletion

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,117 @@
+#!/usr/bin/python3
+"""
+Defines Rectangle class
+"""
+
+class Rectangle:
+    """Define a rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """Initialize a rectangle.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+
+        Raises:
+            TypeError: If width or height is not an integer.
+            ValueError: If width or height is less than zero.
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Retrieve the width of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width of the rectangle.
+
+        Args:
+            value (int): The width of the rectangle.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than zero.
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrieve the height of the rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the height of the rectangle.
+
+        Args:
+            value (int): The height of the rectangle.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than zero.
+        """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Calculate the area of the rectangle.
+
+        Returns:
+            int: The area of the rectangle.
+        """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Calculate the perimeter of the rectangle.
+
+        Returns:
+            int: The perimeter of the rectangle.
+        """
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """Return a string representation of the rectangle using '#' characters.
+
+        Returns:
+            str: A string representation of the rectangle.
+        """
+        if self.__height == 0 or self.__width == 0:
+            return ''
+        ret = ''
+        for i in range(self.__height):
+            for j in range(self.__width):
+                ret += '#'
+            if i < self.__height - 1:
+                ret += '\n'
+        return ret
+
+    def __repr__(self):
+        """Return a string representation that can be used to recreate the object.
+
+        Returns:
+            str: A string representation of the rectangle.
+        """
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    def __del__(self):
+        """Print a message when an instance of Rectangle is deleted."""
+        print("Bye rectangle...")
+
+if __name__ == "__main__":
+    my_rectangle = Rectangle(2, 4)
+    print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+
+    del my_rectangle
--- 0x08-python-more_classes/main.py
+++ 0x08-python-more_classes/main.py
@@ -1,26 +1,12 @@
 #!/usr/bin/python3
-Rectangle = __import__('4-rectangle').Rectangle
+Rectangle = __import__('5-rectangle').Rectangle
 
 my_rectangle = Rectangle(2, 4)
-print(str(my_rectangle))
-print("--")
-print(my_rectangle)
-print("--")
-print(repr(my_rectangle))
-print("--")
-print(hex(id(my_rectangle)))
-print("--")
+print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
 
-# create new instance based on representation
-new_rectangle = eval(repr(my_rectangle))
-print(str(new_rectangle))
-print("--")
-print(new_rectangle)
-print("--")
-print(repr(new_rectangle))
-print("--")
-print(hex(id(new_rectangle)))
-print("--")
+del my_rectangle
 
-print(new_rectangle is my_rectangle)
-print(type(new_rectangle) is type(my_rectangle))
+try:
+    print(my_rectangle)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))

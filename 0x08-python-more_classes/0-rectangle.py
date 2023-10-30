1da552651fb0962954a6eb9afb1a1d0feda215ee
CletsyMedia <Cletussam12@yahoo.com>
2023-10-30 13:04:12 +0100

    An empty class Rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+# Define the Rectangle class
+class Rectangle:
+    pass
+
+# Create an instance of the Rectangle class
+my_rectangle = Rectangle()
+
+# Print the type and attributes of my_rectangle
+print(type(my_rectangle))
+print(my_rectangle.__dict__)

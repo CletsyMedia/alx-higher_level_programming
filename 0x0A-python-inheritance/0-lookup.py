8d1f66615dc1f24cf7a901be9e61b1985fd75ab0
CletsyMedia <Cletussam12@yahoo.com>
2023-11-06 12:49:09 +0100

    Returning list attr and methods of obj

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -5,24 +5,11 @@
 
 
 def lookup(obj):
-  """
+    """Returns the list of available attributes
+    and methods of an object
     Args:
-        obj: The object for which you want to retrieve the attributes and methods.
-
+        obj(obj) - the object to check
     Returns:
-        A list of strings containing the names of attributes and methods associated with the object.
-
-    Example:
-        >>> class MyClass:
-        ...     def __init__(self):
-        ...         self.attribute = 42
-        ...     def method(self):
-        ...         pass
-        ...
-        >>> lookup(MyClass())
-        ['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge',
-        '__getattribute__', '__gt', '__hash__', '__init__', '__le', '__lt', '__ne', '__new', '__reduce',
-        '__reduce_ex', '__repr', '__setattr', '__sizeof', '__str', '__subclasshook', '__weakref',
-        'attribute', 'method']
+        list: list of every attributes and methods
     """
-  return dir(obj)
+    return dir(obj)

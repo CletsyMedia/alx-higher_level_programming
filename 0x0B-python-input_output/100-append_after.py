3fb66e2c9c0f168e9f8403d0577f8d4519112551
CletsyMedia <Cletussam12@yahoo.com>
2023-11-07 21:57:54 +0100

    Search and update

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""a function that inserts a line to a file,
-   after each line containing a specific string
+""" that inserts a line of text to a file, after
+    each line containing a specific string
 """
 
 

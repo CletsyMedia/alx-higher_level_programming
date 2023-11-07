0b7e8d10adaec6c3683d5077f51c907b418a15a5
CletsyMedia <Cletussam12@yahoo.com>
2023-11-07 20:54:09 +0100

    From JSON string to Object

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -9,7 +9,4 @@ def from_json_string(my_str):
     """
         returns an object represented by a JSON string
     """
-    try:
-        return json.loads(my_str)
-    except json.JSONDecodeError as e:
-        return "[{}] {}".format(e.__class__.__name__, e)
+    return json.loads(my_str)

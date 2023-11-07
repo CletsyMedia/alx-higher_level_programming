e04683ce4a615f7bd7c10f44c88daebc495cae50
CletsyMedia <Cletussam12@yahoo.com>
2023-11-07 10:54:33 +0100

    Reads & print a txt file with (UTF8)

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Contains  function that reads a text file
+UTF8) and prints it to stdout"""
+
+
+def read_file(filename=""):
+    """Reads a text file (UTF8) and prints it to stdout
+        Args:
+            filename (str): name of the file
+    """
+    with open(filename, 'r', encoding='utf-8') as file:
+        for line in file:
+            print(line, end='')

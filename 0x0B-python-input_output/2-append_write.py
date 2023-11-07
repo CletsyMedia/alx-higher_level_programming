2fdb18c782bf3ad70f517c13bab3874b15aa65ea
CletsyMedia <Cletussam12@yahoo.com>
2023-11-07 20:20:49 +0100

    Function that appends a string at the end of a text file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -13,9 +13,3 @@ def append_write(filename="", text=""):
     """
     with open(filename, 'a', encoding='utf-8') as file:
         return file.write(text)
-
-
-if __name__ == "__main__":
-    nb_characters_added = append_write(
-        "file_append.txt", "This School is so cool!\n")
-    print(nb_characters_added)

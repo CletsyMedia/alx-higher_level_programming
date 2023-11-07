854c2ba3ff1edff6260ec93942973614e12c40a7
CletsyMedia <Cletussam12@yahoo.com>
2023-11-07 03:46:49 +0100

    Write a class MyInt that inherits from int

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -3,22 +3,12 @@
 
 
 class MyInt(int):
-    """Class representing an integer with inverted equality operators.
+    """Invert int operators == and !=."""
 
-    Args:
-        value (int): The integer value to be stored.
+    def __eq__(self, value):
+        """Override == operator with != behavior."""
+        return self.real != value
 
-    Attributes:
-        value (int): The integer value stored within the object.
-    """
-
-    def __init__(self, value):
-        super().__init__(value)
-
-    def __eq__(self, other):
-        """Override the equality operator (==) with inequality behavior."""
-        return super().__ne__(other)
-
-    def __ne__(self, other):
-        """Override the inequality operator (!=) with equality behavior."""
-        return super().__eq__(other)
+    def __ne__(self, value):
+        """Override != operator with == behavior."""
+        return self.real == value

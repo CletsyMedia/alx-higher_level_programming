588085af400940e740ab5a09cf4df27f9ee3770e
CletsyMedia <Cletussam12@yahoo.com>
2023-11-07 21:39:03 +0100

    Write a class Student that defines a student

--- 0x0B-python-input_output/0-main.py
+++ 0x0B-python-input_output/0-main.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-Student = __import__('9-student').Student
+Student = __import__('10-student').Student
 
-students = [Student("John", "Doe", 23), Student("Bob", "Dylan", 27)]
+student_1 = Student("John", "Doe", 23)
+student_2 = Student("Bob", "Dylan", 27)
 
-for student in students:
-    j_student = student.to_json()
-    print(type(j_student))
-    print(j_student['first_name'])
-    print(type(j_student['first_name']))
-    print(j_student['age'])
-    print(type(j_student['age']))
+j_student_1 = student_1.to_json()
+j_student_2 = student_2.to_json(['first_name', 'age'])
+j_student_3 = student_2.to_json(['middle_name', 'age'])
+
+print(j_student_1)
+print(j_student_2)
+print(j_student_3)

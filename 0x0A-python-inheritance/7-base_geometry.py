fcbc9ef5df2603ae21f9a364691fbe8ade09a04e
CletsyMedia <Cletussam12@yahoo.com>
2023-11-07 03:20:00 +0100

    BaseGeometry (based on 6-base_geometry.py)

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,34 +1,20 @@
 #!/usr/bin/python3
-
-"""Module that defines a class BaseGeometry.
 """
+Holds the class superclass BaseGeometry
+"""
+Base = __import__('6-base_geometry').BaseGeometry
 
-class BaseGeometry:
-    """Class that defines a shape.
+
+class BaseGeometry(Base):
+    """BaseGeometry class containing area and integer_validator
     """
-    def area(self):
-        """
-        Calculate the area of the shape.
-
-        Raises:
-            Exception: Always raises an Exception with the message
-                       "area() is not implemented."
-        """
-        raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """Validates input passed to match conditions provided
         """
-        Validate an integer value.
 
-        Args:
-            name (str): A string representing the name of the value.
-            value (int): The value to be validated.
-
-        Raises:
-            TypeError: If the value is not an integer.
-            ValueError: If the value is less than or equal to 0.
-        """
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError("{:s} must be greater than 0".format(name))
+          

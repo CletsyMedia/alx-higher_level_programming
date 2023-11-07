6f3d2e984a954db763647a006ee36aa7cf5ff0a6
CletsyMedia <Cletussam12@yahoo.com>
2023-11-06 17:55:24 -0800

    Empty BaseGeometry class

--- 0x0A-python-inheritance/3-main.py
+++ 0x0A-python-inheritance/3-main.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-inherits_from = __import__('4-inherits_from').inherits_from
+BaseGeometry = __import__('5-base_geometry').BaseGeometry
 
-a = True
-if inherits_from(a, int):
-    print("{} inherited from class {}".format(a, int.__name__))
-if inherits_from(a, bool):
-    print("{} inherited from class {}".format(a, bool.__name__))
-if inherits_from(a, object):
-    print("{} inherited from class {}".format(a, object.__name__))
+bg = BaseGeometry()
+
+print(bg)
+print(dir(bg))
+print(dir(BaseGeometry))
---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module that defines an empty class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    An empty class called BaseGeometry.
+    """
+    pass
+

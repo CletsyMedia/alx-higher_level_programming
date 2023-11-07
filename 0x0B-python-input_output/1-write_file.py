7f400be1e13515e07215a7ea7b21f003564e5c83
CletsyMedia <Cletussam12@yahoo.com>
2023-11-07 12:10:33 +0100

    writes a string to a txt file &  returns the num of chars written

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -23,3 +23,4 @@ def write_file(filename="", text=""):
 if __name__ == "__main__":
     nb_characters = write_file(
         "my_first_file.txt", "This School is so cool!\n")
+    print(nb_characters)

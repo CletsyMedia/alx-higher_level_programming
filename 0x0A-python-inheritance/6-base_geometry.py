34ae7ee2ec9f2de6a29fd1da40736ed395e85fe9
CletsyMedia <Cletussam12@yahoo.com>
2023-11-07 03:06:48 +0100

    Write a class BaseGeometry (based on 5-base_geometry.py)

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -8,10 +8,10 @@ class BaseGeometry:
     """
     
     def area(self):
-        """
-        Calculate the area of the shape.
-
-        Raises:
+        """Calculate the area of the shape.
+            Returns:
+                float: area
+          Raises:
             Exception: Always raises an Exception with the message
                        "area() is not implemented."
         """

fb5eedebe3179035f95317bc4f112890f7ed087d
CletsyMedia <Cletussam12@yahoo.com>
2023-11-07 21:08:58 +0100

    Create object from a JSON file

--- 0x0B-python-input_output/0-main.py
+++ 0x0B-python-input_output/0-main.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "my_list.json"
-my_list = [1, 2, 3]
-save_to_json_file(my_list, filename)
+my_list = load_from_json_file(filename)
+print(my_list)
+print(type(my_list))
 
 filename = "my_dict.json"
-my_dict = { 
-    'id': 12,
-    'name': "John",
-    'places': [ "San Francisco", "Tokyo" ],
-    'is_active': True,
-    'info': {
-        'age': 36,
-        'average': 3.14
-    }
-}
-save_to_json_file(my_dict, filename)
+my_dict = load_from_json_file(filename)
+print(my_dict)
+print(type(my_dict))
 
 try:
-    filename = "my_set.json"
-    my_set = { 132, 3 }
-    save_to_json_file(my_set, filename)
+    filename = "my_set_doesnt_exist.json"
+    my_set = load_from_json_file(filename)
+    print(my_set)
+    print(type(my_set))
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    filename = "my_fake.json"
+    my_fake = load_from_json_file(filename)
+    print(my_fake)
+    print(type(my_fake))
 except Exception as e:
     print("[{}] {}".format(e.__class__.__name__, e))
---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Contains function that creates an Object from a JSON file"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """Creates an Object from a JSON file:
+        Args:
+            filename (str): name of the file
+        Returns:
+            object: Python data structure loaded from the JSON file
+    """
+    try:
+        with open(filename, 'r', encoding='utf-8') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return None
+    except json.JSONDecodeError:
+        return None

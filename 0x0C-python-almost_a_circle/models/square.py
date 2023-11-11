8042f9e2de798c55a51c1bd021260bf0be71aea5
CletsyMedia <Cletussam12@yahoo.com>
2023-11-11 09:04:06 +0100

    Sqr class inherit from Rec class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -4,11 +4,22 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """This class represents a square"""
+    """A Square class."""
 
     def __init__(self, size, x=0, y=0, id=None):
         """Class constructor"""
         super().__init__(size, size, x, y, id)
+    
+    @property
+    def size(self):
+        """Getter for size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter for size"""
+        self.width = value
+        self.height = value
 
     def __str__(self):
         """Return string representation of the square"""

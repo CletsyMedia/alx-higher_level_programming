073d56fa874971e697f21e1859b623bb9c12d519
CletsyMedia <Cletussam12@yahoo.com>
2023-11-13 20:21:41 +0100

    Updates

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,10 +4,10 @@ from models.base import Base
 
 
 class Rectangle(Base):
-    """This class represents a rectangle"""
+    '''A Rectangle class.'''
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Class constructor"""
+        """Constructor"""
         super().__init__(id)
         self.width = width
         self.height = height

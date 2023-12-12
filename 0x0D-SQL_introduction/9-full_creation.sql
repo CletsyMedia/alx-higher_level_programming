5cb29c799ede1b427c914ad465d72f902fae5ba1
CletsyMedia <Cletussam12@yahoo.com>
2023-12-12 13:04:29 +0100

    Counting id's in a table

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,11 @@
+-- Creating table second_table if it doesn't exist
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+-- Adding rows to the second_table
+INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
+INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
+INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
+INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);

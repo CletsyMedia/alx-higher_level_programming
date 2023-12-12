c0c7ab3a054d686d5e8236d54226b33536f6e0a4
CletsyMedia <Cletussam12@yahoo.com>
2023-12-12 12:28:51 +0100

    Showing tables

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,3 @@
+-- Script to list all tables of a specified database
+-- List all tables in the specified database
+SHOW TABLES;
---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,5 @@
+-- Script to list all rows of the table first_table from the database hbtn_0c_0
+USE hbtn_0c_0;
+-- List all rows from the first_table
+SELECT *
+FROM first_table;

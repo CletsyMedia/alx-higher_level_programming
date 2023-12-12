0879821d92c5ced1faf203b35f0621b9260ff31e
CletsyMedia <Cletussam12@yahoo.com>
2023-12-12 16:53:59 +0100

    Record a score >=10

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,6 @@
+-- Selecting records from the second_table where score is greater than or equal to 10,
+-- ordering by score in descending order
+SELECT score, name
+FROM second_table
+WHERE score >= 10
+ORDER BY score DESC;

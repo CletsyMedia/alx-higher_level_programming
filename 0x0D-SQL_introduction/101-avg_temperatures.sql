8ae609af7ccddee33231f25962f794a368fd9355
CletsyMedia <Cletussam12@yahoo.com>
2023-12-12 17:51:27 +0100

    Import the table dump into the hbtn_0c_0 database

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,5 @@
 -- Import the table dump into the hbtn_0c_0 database
-SELECT city, AVG(temperature) AS avg_temp
-FROM your_imported_table_name
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
 GROUP BY city
 ORDER BY avg_temp DESC;

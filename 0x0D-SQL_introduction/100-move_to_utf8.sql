e945069f43dc4b7e3002f602a5564ef47f5bd274
CletsyMedia <Cletussam12@yahoo.com>
2023-12-12 17:46:01 +0100

    Convert the hbtn_0c_0 database to UTF8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,4 @@
--- Convert the hbtn_0c_0 database to UTF8
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- Convert the first_table table and the name field to UTF8
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Convert the hbtn_0c_0 database and first_table table to UTF8
+USE `hbtn_0c_0`;
+ALTER DATABASE CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE `first_table` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;

2a7eaf22d8347593363174462c71b1947c9739ff
CletsyMedia <Cletussam12@yahoo.com>
2023-12-13 23:55:21 +0100

    task12

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+-- Script to list shows without a linked genre
+/* Selecting shows without a linked genre */
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id;

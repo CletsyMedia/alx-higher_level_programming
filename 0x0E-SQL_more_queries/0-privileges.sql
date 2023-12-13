d2a12dd6ed0958578487f4c2cd0c32af0e774391
CletsyMedia <Cletussam12@yahoo.com>
2023-12-13 19:10:23 +0100

    SQL more queries

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- Script to list all privileges of MySQL users user_0d_1 and user_0d_2 on localhost
+/* Displaying privileges for user_0d_1 */
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+/* Displaying privileges for user_0d_2 */
+SHOW GRANTS FOR 'user_0d_2'@'localhost';

c61f6da6d56f6c10200feccac000a95eb879468d
CletsyMedia <Cletussam12@yahoo.com>
2023-12-13 20:31:13 +0100

    List all cities with associated states using JOIN

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,3 @@
 -- Script to list all cities with associated states using JOIN
 /* Selecting cities with associated states using LEFT JOIN */
-SELECT cities.id, cities.name, states.name
-FROM cities
-LEFT JOIN states ON states.id = cities.state_id
-ORDER BY cities.id;
+SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON states.id = cities.state_id ORDER BY cities.id;

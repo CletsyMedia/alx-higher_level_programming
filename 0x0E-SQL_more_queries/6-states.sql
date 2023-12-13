d3f1c26f9aae0970828937b8475c48301995df7a
CletsyMedia <Cletussam12@yahoo.com>
2023-12-13 19:56:10 +0100

    Create the database hbtn_0d_usa and the table states

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,10 @@
 -- Script to create the database hbtn_0d_usa and the table states
 /* Creating the database if not exists */
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+/* Using the specified database */
+USE hbtn_0d_usa;
 /* Creating the table if not exists */
 CREATE TABLE IF NOT EXISTS states (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(256) NOT NULL
 );

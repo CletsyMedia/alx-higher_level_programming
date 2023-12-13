20ce894fd3595b8307159e095f1bb6d548090b90
CletsyMedia <Cletussam12@yahoo.com>
2023-12-13 19:41:01 +0100

    id_not_null

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,7 @@
+-- Script to create the table id_not_null
+/* Using the specified database  hbtn_0d_2 */
+/* Creating the table if not exists */
+CREATE TABLE IF NOT EXISTS id_not_null (
+    id INT DEFAULT 1,
+    name VARCHAR(256)
+);

f043c0523acebce23f449b954a0eb733a5affc8c
CletsyMedia <Cletussam12@yahoo.com>
2024-01-08 22:14:54 +0100

    Write a script that computes and prints a factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 
-const fact = (n) => {
+const factorial = (n) => {
   if (isNaN(n) || n < 0) {
     return 1;
   }
   if (n === 0) {
     return 1;
   } else {
-    return n * fact(n - 1);
+    return n * factorial(n - 1);
   }
 };
 
 const num = parseInt(process.argv[2]);
 
-console.log(fact(num));
+console.log(factorial(num));

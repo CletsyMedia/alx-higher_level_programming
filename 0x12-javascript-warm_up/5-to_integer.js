b27f3a8ab256d512e1c416a475720d9b6c2e445b
CletsyMedia <Cletussam12@yahoo.com>
2024-01-08 14:32:04 +0100

    Write a script that prints My number or Not a number

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,4 +2,8 @@
 
 const num = parseInt(process.argv[2]);
 
-if (!isNaN(num)) { console.log('My number:', num); } else { console.log('Not a number:'); }
+if (!isNaN(num))
+  console.log('My number:', num);
+else{
+  console.log('Not a number:');
+}

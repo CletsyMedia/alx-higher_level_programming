f01ce042069261dd8e513a508c9eefc51852bcb6
CletsyMedia <Cletussam12@yahoo.com>
2024-01-08 14:43:36 +0100

    Write a script that prints 3 lines: (like 1-multi_languages.js)

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (const language of languages) {
   console.log(language);

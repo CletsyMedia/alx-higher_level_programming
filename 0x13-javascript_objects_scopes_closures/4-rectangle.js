44e3ab585e149093be97229ed60b39e495199c9d
CletsyMedia <Cletussam12@yahoo.com>
2024-01-09 18:46:56 +0100

    Rect#4 rotate and double

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor(w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      Object.create(null);
+    }
+  }
+
+  print() {
+    if (this.width !== undefined && this.height !== undefined) {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      }
+    }
+  }
+}
+
+module.exports = Rectangle;

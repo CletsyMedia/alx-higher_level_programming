5adcbae0c3404c260a5c0d07cca502891e448de5
CletsyMedia <Cletussam12@yahoo.com>
2024-01-09 18:18:13 +0100

    Write a class Rectangle that defines a rectangle

--- 0x13-javascript_objects_scopes_closures/main.js
+++ 0x13-javascript_objects_scopes_closures/main.js
@@ -1,17 +1,7 @@
 #!/usr/bin/node
+
 const Rectangle = require('./1-rectangle');
-
-const r1 = new Rectangle(2, 3);
-console.log(r1);
-console.log(r1.width);
-console.log(r1.height);
-
-const r2 = new Rectangle(2, -3);
-console.log(r2);
-console.log(r2.width);
-console.log(r2.height);
-
-const r3 = new Rectangle(2);
-console.log(r3);
-console.log(r3.width);
-console.log(r3.height);
+const rec = new Rectangle(4, 5);
+console.log(rec);
+console.log(rec.width);
+console.log(rec.height);

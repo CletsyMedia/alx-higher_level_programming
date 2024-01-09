666a4732f4bc664444ceeeea2e3a7ea0437f145a
CletsyMedia <Cletussam12@yahoo.com>
2024-01-09 19:02:25 +0100

    Create an instance method called charPrint(c)

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+
+const Rectangle = require('./5-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    // Call the constructor of the parent class (Rectangle)
+    super(size, size);
+  }
+}

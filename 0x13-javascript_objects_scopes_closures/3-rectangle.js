6f423bb68a926b0d53735cec4ec5683165cbcae6
CletsyMedia <Cletussam12@yahoo.com>
2024-01-09 18:42:11 +0100

    Prints the rectangle using the character X

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,12 +1,20 @@
 #!/usr/bin/node
 
 class Rectangle {
-  constructor (w, h) {
+  constructor(w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     } else {
       Object.create(null);
+    }
+  }
+
+  print() {
+    if (this.width !== undefined && this.height !== undefined) {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      }
     }
   }
 }

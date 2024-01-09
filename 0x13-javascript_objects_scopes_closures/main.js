95ccc984d1f04166c14fc899f7ba6b048a7ef829
CletsyMedia <Cletussam12@yahoo.com>
2024-01-09 18:28:36 +0100

    Rectangle #2

--- 0x13-javascript_objects_scopes_closures/main.js
+++ 0x13-javascript_objects_scopes_closures/main.js
@@ -1,7 +1,22 @@
 #!/usr/bin/node
+const Rectangle = require('./2-rectangle');
 
-const Rectangle = require('./1-rectangle');
-const rec = new Rectangle(4, 5);
-console.log(rec);
-console.log(rec.width);
-console.log(rec.height);
+const r1 = new Rectangle(2, 3);
+console.log(r1);
+console.log(r1.width);
+console.log(r1.height);
+
+const r2 = new Rectangle(2, -3);
+console.log(r2);
+console.log(r2.width);
+console.log(r2.height);
+
+const r3 = new Rectangle(2);
+console.log(r3);
+console.log(r3.width);
+console.log(r3.height);
+
+const r4 = new Rectangle(2, 0);
+console.log(r4);
+console.log(r4.width);
+console.log(r4.height);

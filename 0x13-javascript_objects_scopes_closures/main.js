f7b5c6c22e15c9f9e26374e6021a0df636492712
CletsyMedia <Cletussam12@yahoo.com>
2024-01-09 19:01:19 +0100

    Create an instance method called charPrint(c)

--- 0x13-javascript_objects_scopes_closures/main.js
+++ 0x13-javascript_objects_scopes_closures/main.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-const Rectangle = require('./3-rectangle');
+const Square = require('./5-square');
 
-const r1 = new Rectangle(2, 3);
-r1.print();
-
-const r2 = new Rectangle(10, 5);
-r2.print();
+const s1 = new Square(4);
+s1.print();
+s1.double();
+s1.print();

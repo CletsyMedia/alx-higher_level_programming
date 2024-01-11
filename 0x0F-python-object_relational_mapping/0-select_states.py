2c53ef1e5655cc885a20df2e83731ff3c9bfd8be
CletsyMedia <Cletussam12@yahoo.com>
2024-01-11 20:30:11 +0100

    Script that lists all states from the database

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Script that lists all states from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+import sys
+
+def select_states(username, password, db_name):
+    """Selects all states from the specified database and prints the results"""
+
+    # Connect to MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        user=username,
+        passwd=password,
+        db=db_name,
+        port=3306
+    )
+
+    # Create a cursor object to interact with the database
+    cursor = db.cursor()
+
+    # Execute the SELECT query
+    cursor.execute("SELECT * FROM states ORDER BY id")
+
+    # Fetch all the rows
+    rows = cursor.fetchall()
+
+    # Print the results
+    for row in rows:
+        print(row)
+
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
+
+if __name__ == "__main__":
+    # Check if the correct number of arguments is provided
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Extract command line arguments
+    username, password, db_name = sys.argv[1:]
+
+    # Call the select_states function
+    select_states(username, password, db_name)

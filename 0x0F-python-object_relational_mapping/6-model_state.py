6f7b3f32475f18cbf419880b3e4466bda571708f
CletsyMedia <Cletussam12@yahoo.com>
2024-01-12 18:27:12 +0100

    First state model

--- 0x0F-python-object_relational_mapping/6-model_state.py
+++ 0x0F-python-object_relational_mapping/6-model_state.py
@@ -12,11 +12,3 @@ if __name__ == "__main__":
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
-
-    session = Session(engine)
-
-    new_state = State(name='California')
-    session.add(new_state)
-    session.commit()
-
-    session.close()

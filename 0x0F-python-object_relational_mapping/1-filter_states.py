64ebc32c58eb551d6e08a57a597fc6e74858355d
CletsyMedia <Cletussam12@yahoo.com>
2024-01-12 11:12:26 +0100

    Lists all states with a name starting with N (upper N) from the database

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Lists all states with a name starting with N from the database hbtn_0e_0_usa
-"""
+""" Lists all states from the database hbtn_0e_0_usa """
 
 import MySQLdb
 import sys

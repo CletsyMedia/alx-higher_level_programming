d8499cdc3f95422a6b36bd266981f3d3eee9f856
CletsyMedia <Cletussam12@yahoo.com>
2024-01-12 18:47:42 +0100

    First state model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""Start link class to table in database 
+"""Start link class to table in database
 """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
 
 class State(Base):
     """Class representing a state."""

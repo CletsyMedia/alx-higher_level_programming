77d0f887aa1ed1691192776ab0b05126bd789235
CletsyMedia <Cletussam12@yahoo.com>
2024-01-12 18:42:21 +0100

    First state model

--- 0x0F-python-object_relational_mapping/6-model_state.py
+++ 0x0F-python-object_relational_mapping/6-model_state.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""Start link class to table in database
+"""Start link class to table in database 
 """
 import sys
 from model_state import Base, State
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'

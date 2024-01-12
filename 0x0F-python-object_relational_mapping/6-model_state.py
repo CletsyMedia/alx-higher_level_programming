9da26217d67665803f9a553b754de56d240b7c65
CletsyMedia <Cletussam12@yahoo.com>
2024-01-12 18:22:56 +0100

    First state model

---
+++ 0x0F-python-object_relational_mapping/6-model_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Start link class to table in database 
+"""
+import sys
+from model_state import Base, State
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+
+    new_state = State(name='California')
+    session.add(new_state)
+    session.commit()
+
+    session.close()

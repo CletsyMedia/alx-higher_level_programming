4acd5b8d3923d1dd87097e2c07adb9a3128bb1c3
CletsyMedia <Cletussam12@yahoo.com>
2024-01-25 20:11:27 +0100

    Script that takes in a URL, sends a GET request to the URL

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Takes a URL, sends a GET request using curl, and displays the body for a 200 status code.
-response=$(curl -sL -w "%{http_code}" "$1" -o /dev/null)
-if [ "$response" -eq 200 ]; then curl -sL "$1"; fi
+# This script takes a URL as an argument, sends a GET request using curl, and displays the body of the response for a 200 status code.
+curl -sL -w "%{http_code}" "$1" -o /dev/null

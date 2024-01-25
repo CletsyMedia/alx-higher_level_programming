fcadcbdecdbdadec839643ed47f19f7cb0146524
CletsyMedia <Cletussam12@yahoo.com>
2024-01-25 19:58:43 +0100

    Add Bash script to fetch and display response body size using curl

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # This script takes a URL as an argument, sends a request using curl, and displays the size of the response body in bytes.
-
-curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}' | tr -d '\r'

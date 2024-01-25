89a5c87a538ae601a56ccad3b6df78e2a95af218
CletsyMedia <Cletussam12@yahoo.com>
2024-01-25 22:38:14 +0100

    Bash script that sends a JSON POST request to a URL

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 # Sends a JSON POST request to a URL with the contents of a file in the body, and displays the body of the response.
-json_file="$2"
-
-if [ -f "$json_file" ]; then
-    curl -sX POST -H "Content-Type: application/json" -d @"$json_file" "$1"
-else
-    echo "File not found or is not a regular file."
-fi
+curl -s -X POST "$1" -H "Content-Type: application/json" -d @"$2"

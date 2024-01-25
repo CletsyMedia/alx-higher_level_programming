052cacd58fe56a99f8a2c2d27c9cf839399dc84f
CletsyMedia <Cletussam12@yahoo.com>
2024-01-25 21:45:34 +0100

    Tkes in a URL and displays all HTTP methods the server will accept

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes a URL and displays all HTTP methods the server will accept.
-curl -sI -X OPTIONS "$1" | grep -i Allow | awk '{print $2}'
+curl -sI -X OPTIONS "$1" | grep -i Allow | cut -d ' ' -f 2-

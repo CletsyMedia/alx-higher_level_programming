9453767597a3866ea8745f1129b9ff0ea926f3c3
CletsyMedia <Cletussam12@yahoo.com>
2024-01-26 14:07:17 +0100

    Takes a URL, sends a req to the URL and display

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -9,11 +9,15 @@ import urllib.error
 import sys
 
 if __name__ == "__main__":
+    # Get the URL from the command-line argument
     url = sys.argv[1]
 
     try:
+        # Try to open the URL and read the response
         with urllib.request.urlopen(url) as response:
+            # Decode the response body in utf-8 and print it
             body = response.read().decode('utf-8')
             print(body)
     except urllib.error.HTTPError as e:
+        # Handle HTTP errors by printing the error code
         print("Error code: {}".format(e.code))

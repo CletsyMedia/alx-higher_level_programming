d89e7f0b508b2d7d65c27260397d4ccc21b3cbdb
CletsyMedia <Cletussam12@yahoo.com>
2024-01-26 22:00:27 +0100

    URL, sends a request to the URL and displays the body

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,24 +1,18 @@
 #!/usr/bin/python3
 """
-Python script that takes in a URL, sends a request to the URL
+Python script that takes in a URL, sends a request to the URL,
 and displays the body of the response.
 If the HTTP status code is greater than or equal to 400,
 print: Error code: followed by the value of the HTTP status code
 """
 
-import requests
-import sys
+from requests import get
+from sys import argv
 
 if __name__ == "__main__":
-    # Get the URL from command line arguments
-    url = sys.argv[1]
+    response = get(argv[1])
 
-    # Send a request to the URL
-    response = requests.get(url)
-
-    # Display the body of the response
-    print(response.text)
-
-    # Check if the status code is greater than or equal to 400
     if response.status_code >= 400:
         print("Error code: {}".format(response.status_code))
+    else:
+        print(response.text)

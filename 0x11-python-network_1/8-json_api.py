e266c054d72558b49a545263cf6cb250126c71b9
CletsyMedia <Cletussam12@yahoo.com>
2024-01-26 22:07:21 +0100

    Searching API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,21 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
 
+import requests
+import sys
+
+if __name__ == "__main__":
+    # Set the default value for the letter
+    q = "" if len(sys.argv) == 1 else sys.argv[1]
+
+    # Make the POST request with the letter as a parameter
+    response = requests.post("http://0.0.0.0:5000/search_user", data={'q': q})
+
+    try:
+        # Try to parse the JSON response
+        json_data = response.json()
+
+        # Check if the JSON

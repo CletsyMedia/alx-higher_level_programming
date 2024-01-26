ad60bd723262cd8278a28bfc25b46badc7b4c151
CletsyMedia <Cletussam12@yahoo.com>
2024-01-26 11:11:20 +0100

    Script that takes in a URL, sends a request to the URL

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module to fetch https://alx-intranet.hbtn.io/status
+"""
+
+import urllib.request
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
+        print("Body response:")
+        print(f"    - type: {type(content)}")
+        print(f"    - content: {content}")
+        print(f"    - utf8 content: {content.decode('utf-8')}")

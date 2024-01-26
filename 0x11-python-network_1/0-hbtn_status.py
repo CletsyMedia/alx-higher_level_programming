ae11b19077c2085cf3620da1887a2df23366bb38
CletsyMedia <Cletussam12@yahoo.com>
2024-01-26 12:31:31 +0100

    Module to fetch https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen(url) as response:
         content = response.read()
         print("Body response:")
-        print(f"    - type: {type(content)}")
-        print(f"    - content: {content}")
-        print(f"    - utf8 content: {content.decode('utf-8')}")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))

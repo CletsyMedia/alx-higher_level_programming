703b35ee0da31f1a5723c606acc714365e07d622
CletsyMedia <Cletussam12@yahoo.com>
2024-02-20 15:28:55 +0100

    Script that gets the contents of a webpage and stores it in a file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,21 +3,15 @@
 const request = require('request');
 const fs = require('fs');
 
-const url = process.argv[2];
-const filePath = process.argv[3];
+const [url, filePath] = process.argv.slice(2);
 
 request(url, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
+  if (error || response.statusCode !== 200) {
+    console.error('Error:', error || 'Invalid response:', response.statusCode);
     process.exit(1);
   }
 
-  if (response.statusCode !== 200) {
-    console.error('Invalid response:', response.statusCode);
-    process.exit(1);
-  }
-
-  fs.writeFile(filePath, body, 'utf-8', (err) => {
+  fs.writeFile(filePath, body, 'utf-8', err => {
     if (err) {
       console.error('Error writing to file:', err);
       process.exit(1);

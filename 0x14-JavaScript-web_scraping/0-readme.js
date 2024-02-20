63ae8c9a2bce1b087c1e7f8dd6a58a52062c43d7
CletsyMedia <Cletussam12@yahoo.com>
2024-02-20 05:48:30 +0100

    Reading the content of a file using JS

---
+++ 0x14-JavaScript-web_scraping/0-readme.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+// Check if a file path is provided as a command-line argument
+if (process.argv.length !== 3) {
+    console.error('Usage: ./read_file.js <file_path>');
+    process.exit(1);
+}
+
+const filePath = process.argv[2];
+
+// Read the content of the file asynchronously
+fs.readFile(filePath, 'utf8', function(error, content) {
+    if (error) {
+        console.error('Error reading file:', error);
+        process.exit(1);
+    }
+    console.log(content);
+});
+

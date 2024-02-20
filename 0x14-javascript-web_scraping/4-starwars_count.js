42396d2c5bf89009d72f2587218401da83e0c022
CletsyMedia <Cletussam12@yahoo.com>
2024-02-20 15:06:10 +0100

    Script that prints the number of movies where the character “Wedge Antilles” is present

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -10,6 +10,11 @@ request(apiUrl, function (error, response, body) {
     return;
   }
 
+  if (response.statusCode !== 200) {
+    console.error('Invalid response:', response.statusCode);
+    return;
+  }
+
   const films = JSON.parse(body).results;
   const wedgeAntillesID = '18';
 

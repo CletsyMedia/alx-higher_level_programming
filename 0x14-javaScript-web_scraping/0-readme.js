cbd580ed854e2adaf945f5aa22405765e38b28e1
CletsyMedia <Cletussam12@yahoo.com>
2024-02-20 08:23:53 +0100

    Updating a script that reads and prints the content of a file

--- 0x14-javaScript-web_scraping/0-readme.js
+++ 0x14-javaScript-web_scraping/0-readme.js
@@ -4,18 +4,17 @@ const fs = require('fs');
 
 // Check if a file path is provided as a command-line argument
 if (process.argv.length !== 3) {
-    console.error('Usage: ./read_file.js <file_path>');
-    process.exit(1);
+  console.error('Usage: ./read_file.js <file_path>');
+  process.exit(1);
 }
 
 const filePath = process.argv[2];
 
 // Read the content of the file asynchronously
-fs.readFile(filePath, 'utf8', function(error, content) {
-    if (error) {
-        console.error(error);
-        process.exit(1);
-    }
-    console.log(content);
+fs.readFile(filePath, 'utf8', function (error, content) {
+  if (error) {
+    console.error(error);
+    process.exit(1);
+  }
+  console.log(content);
 });
-

8f3aa61bf4eb3d8978c41da0505481a8dad18737
CletsyMedia <Cletussam12@yahoo.com>
2024-02-20 15:16:21 +0100

    Script that prints the number of movies where the character “Wedge Antilles” is present

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,25 +2,33 @@
 
 const request = require('request');
 
-const apiUrl = process.argv[2];
+function countMoviesWithCharacter (apiUrl, characterID, callback) {
+  request(apiUrl, function (error, response, body) {
+    if (error) {
+      return callback(error);
+    }
 
-request(apiUrl, function (error, response, body) {
+    if (response.statusCode !== 200) {
+      return callback(new Error('Invalid response: ' + response.statusCode));
+    }
+
+    const films = JSON.parse(body).results;
+    const moviesWithCharacter = films.filter(film =>
+      film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterID}/`)
+    );
+
+    callback(null, moviesWithCharacter.length);
+  });
+}
+
+const apiUrl = process.argv[2];
+const characterID = '18';
+
+countMoviesWithCharacter(apiUrl, characterID, function (error, count) {
   if (error) {
     console.error('Error:', error);
-    return;
+    process.exit(1);
   }
 
-  if (response.statusCode !== 200) {
-    console.error('Invalid response:', response.statusCode);
-    return;
-  }
-
-  const films = JSON.parse(body).results;
-  const wedgeAntillesID = '18';
-
-  const moviesWithWedge = films.filter(film =>
-    film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${wedgeAntillesID}/`)
-  );
-
-  console.log(moviesWithWedge.length);
+  console.log(count);
 });

490635206d53b3004750cc17c50ebb659f9daed2
CletsyMedia <Cletussam12@yahoo.com>
2024-02-20 15:25:59 +0100

    Script that gets the contents of a webpage and stores it in a file

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    process.exit(1);
+  }
+
+  if (response.statusCode !== 200) {
+    console.error('Invalid response:', response.statusCode);
+    process.exit(1);
+  }
+
+  fs.writeFile(filePath, body, 'utf-8', (err) => {
+    if (err) {
+      console.error('Error writing to file:', err);
+      process.exit(1);
+    }
+    console.log(`The content has been saved to ${filePath}`);
+  });
+});

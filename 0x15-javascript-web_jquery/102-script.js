746f73b648e6cd103cc41649e4a57db69052b5ea
CletsyMedia <Cletussam12@yahoo.com>
2024-02-28 21:23:43 +0100

    updates

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $('#btn_translate').click(function(){
+    const languageCode = $('#language_code').val();
+    $.get(`https://www.fourtonfish.com/hellosalut/hello/?lang=${languageCode}`, function(data) {
+      $('#hello').text(data.hello);
+    });
+  });
+});
+
